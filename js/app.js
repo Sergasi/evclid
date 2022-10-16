41cbe8c1aa29274f2f1b0fa9a4848a12f89cd130
Sergasi <a.mikhailyuk98@gmail.com>
2022-10-16 19:44:59 +0500

    init

---
+++ js/app.js
@@ -0,0 +1,9 @@
+const header = document.getElementById('header')
+const burger = document.getElementById('burger')
+const burgerClose = document.getElementsByClassName('x')
+burger.addEventListener('click', () => {
+    header.classList.add('opened')
+})
+burgerClose[0].addEventListener('click', () => {
+    header.classList.remove('opened')
+})
